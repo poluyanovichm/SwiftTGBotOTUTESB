0cabebf4ae542257c3fcd1f2b1219c5743595197
Mikhail <mon_doc@MacBook-Air-Mikhail.local>
2022-12-13 09:37:08 +0300

    13.12.2022 second

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,21 @@
 import Vapor
+import telegram_vapor_bot
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    app.http.server.configuration.hostname = "0.0.0.0"
+    app.http.server.configuration.port = 80
+    
+    
+    let tgApi: String = "5989670598:AAGC7ZLs8j4d55AaiYHwixSpCJWo54pJBbE"
+    let connection: TGConnectionPrtcl = TGLongPollingConnection()
+    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
+    try TGBot.shared.start()
+    TGBot.log.logLevel = .error
+    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+    
     // register routes
     try routes(app)
 }
